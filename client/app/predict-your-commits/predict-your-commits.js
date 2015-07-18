88128c0dc89c710dd845effdbfcaa6b2b3c1e5b2
joeyism <sham.joey@gmail.com>
2015-07-18 19:38:36 -0400

    Fixed up styling

--- client/app/predict-your-commits/predict-your-commits.js
+++ client/app/predict-your-commits/predict-your-commits.js
@@ -1,6 +1,11 @@
 "use strict";
 
 angular.module('predictYourCommitsApp').directive("predictYourCommits", ["$http", function($http){
+    var textColor = "#F3C110";
+    var backgroundColor = "#393531";
+    var legendTextColor = "#B5A6D3";
+    var legendHoverColor = "#F1D9FB";
+    var legendHiddenColor = "#1D2F43";
 
     return {
         templateUrl: "app/predict-your-commits/predict-your-commits.tpl.html",
@@ -10,6 +15,12 @@ angular.module('predictYourCommitsApp').directive("predictYourCommits", ["$http"
         },
         link: function(scope){
             scope.loaded = false;
+            var textStyle = {
+                color: textColor
+            };
+            var axisLabelStyle = {
+                style: textStyle
+            };
 
             $http.get("/api/user/"+scope.username).success(function(commits){
                 scope.loaded = true;
@@ -23,32 +34,50 @@ angular.module('predictYourCommitsApp').directive("predictYourCommits", ["$http"
                 scope.chartConfig = {
                     options: {
                         chart: {
-                            type: 'spline'
+                            type: 'spline',
+                            backgroundColor: backgroundColor,
+                            color: textColor
                         },
                         tooltip: {
                             style: {
                                 padding: 10,
                                 fontWeight: 'bold'
                             },
-                            pointFormat: 'Commits to {series.name}: {point.y:f}',
+                            pointFormat: 'Commits to <span style="color: {series.color}">{series.name}</span>: {point.y:f}',
                             xDateFormat: '%Y-%m-%d',
-                        }
+                        },
+                        legend: {
+                            itemStyle: {
+                                color: legendTextColor
+                            },
+                            itemHoverStyle: {
+                                color: legendHoverColor
+                            },
+                            itemHiddenStyle: {
+                                color: legendHiddenColor
+                            }
+                        },
                     },
                     series: dataSeries,
                     title: {
-                        text: "Number of commits by " + scope.username
+                        text: "Number of commits by " + scope.username,
+                        style: textStyle
                     },
                     xAxis: {
                         type: 'datetime',
                         title: {
-                            text: 'Date'
-                        }
+                            text: 'Date',
+                            style: textStyle
+                        },
+                        labels: axisLabelStyle
                     },
                     yAxis: {
                         title: {
-                            text: 'Number of Commits'
+                            text: 'Number of Commits',
+                            style: textStyle 
                         },
-                        min: 0
+                        min: 0,
+                        labels: axisLabelStyle
                     },
                     loading: false
                 }; 
